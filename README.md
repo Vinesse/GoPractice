099f4af843c321816794d41ce61e4bf1511817d5
Vanessa Aguilar <vanessa.aguilar@ecosia.org>
2020-11-11 22:54:50 +0100

    error handling, single method support, update readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Infra Assignment 👩🏽‍🔧✨
 
 Hello, World 👋🏽
+
+Here we are creating a simple web server with a single endpoint of `/tree`.
+
+When requested, the response `Tree!` is served.
+
+It only supports a single `GET` method.
--- server.go
+++ server.go
@@ -5,11 +5,22 @@ import (
     "log"
     "net/http"
 )
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/tree" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "405 method is not supported.", http.StatusNotFound)
+        return
+	}
+	
+    fmt.Fprintf(w, "Tree!")
+}
 
 func main() {
-    http.HandleFunc("/tree", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Trees :]")
-    })
+    http.HandleFunc("/tree", helloHandler)
 
 
 	fmt.Printf("Starting server at port 8080\n")
